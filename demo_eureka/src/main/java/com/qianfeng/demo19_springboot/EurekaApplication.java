0b54e5d3a1766cd762f13fc730d44bb6b551f4fb
mingmingzou <1355295032@qq.com>
2019-05-15 08:55:29 +0800

    cddd

---
+++ demo_eureka/src/main/java/com/qianfeng/demo19_springboot/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.qianfeng.demo19_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// EnableEurekaServer注解表示将当前项目作为注册中心启动
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
+
