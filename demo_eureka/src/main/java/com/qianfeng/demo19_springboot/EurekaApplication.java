f0bc859f4e9e1c30f114ebbc751992d5f9358c53
mingmingzou <1355295032@qq.com>
2019-05-15 10:39:14 +0800

    第N+N次上传

--- demo_eureka/src/main/java/com/qianfeng/demo19_springboot/EurekaApplication.java
+++ demo_eureka/src/main/java/com/qianfeng/demo19_springboot/EurekaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 // EnableEurekaServer注解表示将当前项目作为注册中心启动
 //邹明明大佬带我飞啊
 @EnableEurekaServer
+//好的呀
 @SpringBootApplication//sdasdasdadsa
 public class EurekaApplication {
 //大苏打实打实大苏打倒萨交付给事件的发生刚刚热辣黄山警方开始开发是会计法上空的飞机看点士肯德基公共机构尽快发给杀毒过速度快是国家讲课
